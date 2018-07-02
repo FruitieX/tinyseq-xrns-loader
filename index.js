8dfcbf67fadc0f1040be68119e35a17b906d9bc5
Rasmus Eskola <fruitiex@gmail.com>
2018-07-02 18:54:10 +0300

    Load instruments from separate file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const { join, dirname } = require('path');
 const { xrns2ts } = require('tinyseq');
 
 module.exports = function(source) {
   const callback = this.async();
+  const instrumentsFilePath = join(dirname(this.resourcePath), 'instruments');
+  const instruments = require(instrumentsFilePath);
 
-  xrns2ts(source, result => {
+  xrns2ts(source, instruments, result => {
     // what could possibly go wrong
     eval(`result=${result}`);
     callback(null, result);
